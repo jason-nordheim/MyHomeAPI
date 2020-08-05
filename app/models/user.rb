33747b186df97128cabeeffb8994bbd0341ce4f3
jason-nordheim <jason.nordheim@gmail.com>
2020-08-05 17:40:12 -0600

    added additional associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :Items 
-  has_many :Vendors 
+  has_many :items 
+  has_many :vendors 
+  has_many :locations 
   validates :username, uniqueness: true 
   validates :email, uniqueness: true 
 end
